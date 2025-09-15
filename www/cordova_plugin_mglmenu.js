4c51d334626d0f152f7de4acbfe7981141635f63
rsdeveloperhx <82517490+rsdeveloperhx@users.noreply.github.com>
2025-09-16 00:42:47 +0200

    Update cordova_plugin_mglmenu.js

--- www/cordova_plugin_mglmenu.js
+++ www/cordova_plugin_mglmenu.js
@@ -1,10 +1,11 @@
 var exec = require('cordova/exec');
 
-var MenuPlugin = {
-    createMenuItem: function(id, title, success, error) {
-        exec(success, error, "MenuPlugin", "createMenuItem", [{id: id, title: title}]);
-    },
-    showMenu: function(success, error) {
-        exec(success, error, "MenuPlugin", "showMenu", []);
-    }
-};
+function showMenu() {
+    exec(
+        function(success) { console.log(success); },
+        function(error) { console.error(error); },
+        'MenuPlugin',
+        'showMenu',
+        []
+    );
+}
