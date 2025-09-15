86493c6960f931aa92c88925b6e05f6d04ff49bc
rsdeveloperhx <82517490+rsdeveloperhx@users.noreply.github.com>
2025-09-16 00:58:46 +0200

    Update cordova_plugin_mglmenu.js

--- www/cordova_plugin_mglmenu.js
+++ www/cordova_plugin_mglmenu.js
@@ -1,11 +1,25 @@
 var exec = require('cordova/exec');
 
-function showMenu() {
-    exec(
-        function(success) { console.log(success); },
-        function(error) { console.error(error); },
-        'MenuPlugin',
-        'showMenu',
-        []
-    );
-}
+var cordova_plugin_mglmenu = {
+    // Ein Menüitem erstellen (id und Titel)
+    createMenuItem: function(id, title, success, error) {
+        exec(success, error, "MenuPlugin", "createMenuItem", [{ id: id, title: title }]);
+    },
+
+    // Menü anzeigen / aktualisieren
+    showMenu: function(success, error) {
+        exec(success, error, "MenuPlugin", "showMenu", []);
+    },
+
+    // Menüitems zurücksetzen (optional)
+    clearMenu: function(success, error) {
+        exec(success, error, "MenuPlugin", "clearMenu", []);
+    }
+};
+
+// Event für Klicks auf Menüitems
+document.addEventListener('pluginMenuClicked', function(e) {
+    console.log('Menu item clicked with id:', e.id);
+});
+
+module.exports = cordova_plugin_mglmenu;
