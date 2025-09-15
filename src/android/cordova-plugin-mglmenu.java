31d9d2b425bd04c1e6e3d811911ba70f907e7a94
rsdeveloperhx <82517490+rsdeveloperhx@users.noreply.github.com>
2025-09-16 00:05:42 +0200

    Update cordova-plugin-mglmenu.java

--- src/android/cordova-plugin-mglmenu.java
+++ src/android/cordova-plugin-mglmenu.java
@@ -1,7 +1,15 @@
 package ch.migros.plugin;
 
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,21 +20,48 @@ import org.json.JSONObject;
  */
 public class cordova_plugin_mglmenu extends CordovaPlugin {
 
+    private static CordovaWebView sWebView;
+    private static List<String> menuItems = new ArrayList<>();
+
+    @Override
+    public void initialize(org.apache.cordova.CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        sWebView = webView;
+    }
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if ("addMenuItem".equals(action)) {
+            String item = args.getString(0);
+            menuItems.add(item);
+            cordova.getActivity().runOnUiThread(() -> {
+                cordova.getActivity().invalidateOptionsMenu(); // refresh menu
+            });
+            callbackContext.success("Menu item added: " + item);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        for (String item : menuItems) {
+            menu.add(item);
         }
+        menu.add("Help");  // optional default items
+        menu.add("About");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String title = item.getTitle().toString();
+        Log.d("NativeMenuPlugin", "Menu selected: " + title);
+
+        if (sWebView != null) {
+            sWebView.loadUrl("javascript:window.NativeMenuPlugin.menuSelected('" + title + "');");
+        }
+
+        return true;
     }
 }
