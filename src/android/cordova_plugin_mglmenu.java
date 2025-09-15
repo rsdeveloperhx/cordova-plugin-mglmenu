94af3f0c9673e17634b3022a3416680b958765cb
rsdeveloperhx <82517490+rsdeveloperhx@users.noreply.github.com>
2025-09-16 00:45:22 +0200

    Update cordova_plugin_mglmenu.java

--- src/android/cordova_plugin_mglmenu.java
+++ src/android/cordova_plugin_mglmenu.java
@@ -9,16 +9,56 @@ import android.view.MenuItem;
 
 public class cordova_plugin_mglmenu extends CordovaPlugin {
 
-    private Menu menu; 
+    private Menu menu;
+    private List<MenuItemData> menuItems = new ArrayList<>();
+
+    private static class MenuItemData {
+        int id;
+        String title;
+
+        MenuItemData(int id, String title) {
+            this.id = id;
+            this.title = title;
+        }
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("showMenu".equals(action)) {
+        if ("createMenuItem".equals(action)) {
+            JSONObject obj = args.getJSONObject(0);
+            int id = obj.getInt("id");
+            String title = obj.getString("title");
+            menuItems.add(new MenuItemData(id, title));
+            callbackContext.success("MenuItem added");
+            return true;
+        } else if ("showMenu".equals(action)) {
             cordova.getActivity().runOnUiThread(() -> {
                 cordova.getActivity().invalidateOptionsMenu();
             });
-            callbackContext.success("Menu updated");
+            callbackContext.success("Menu shown");
             return true;
+        }
+        return false;
+    }
+
+    // Wird von MainActivity weitergeleitet
+    public void onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        menu.clear(); // vorherige Items löschen
+
+        for (MenuItemData itemData : menuItems) {
+            MenuItem item = menu.add(Menu.NONE, itemData.id, Menu.NONE, itemData.title);
+            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        }
+    }
+
+    // Wird von MainActivity weitergeleitet
+    public boolean onOptionsItemSelected(MenuItem item) {
+        for (MenuItemData menuItem : menuItems) {
+            if (menuItem.id == item.getItemId()) {
+                webView.loadUrl("javascript:cordova.fireDocumentEvent('pluginMenuClicked', {id:" + menuItem.id + "});");
+                return true;
+            }
         }
         return false;
     }
