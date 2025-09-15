a232e0547be0936c482698e2219b8f975bc76d79
rsdeveloperhx <82517490+rsdeveloperhx@users.noreply.github.com>
2025-09-16 00:56:10 +0200

    Update cordova_plugin_mglmenu.java

--- src/android/cordova_plugin_mglmenu.java
+++ src/android/cordova_plugin_mglmenu.java
@@ -3,64 +3,58 @@ package ch.migros.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class cordova_plugin_mglmenu extends CordovaPlugin {
+import java.util.ArrayList;
+import java.util.List;
 
+public class cordova_plugin_mglmenu extends CordovaPlugin {
+    private static cordova_plugin_mglmenu instance;
     private Menu menu;
     private List<MenuItemData> menuItems = new ArrayList<>();
 
-    private static class MenuItemData {
-        int id;
-        String title;
-
-        MenuItemData(int id, String title) {
-            this.id = id;
-            this.title = title;
-        }
+    public cordova_plugin_mglmenu() {
+        instance = this;
     }
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("createMenuItem".equals(action)) {
-            JSONObject obj = args.getJSONObject(0);
-            int id = obj.getInt("id");
-            String title = obj.getString("title");
-            menuItems.add(new MenuItemData(id, title));
-            callbackContext.success("MenuItem added");
-            return true;
-        } else if ("showMenu".equals(action)) {
+    public static cordova_plugin_mglmenu getInstance() {
+        return instance;
+    }
+
+    public void addMenuItem(int id, String title) {
+        menuItems.add(new MenuItemData(id, title));
+        if (menu != null) {
             cordova.getActivity().runOnUiThread(() -> {
                 cordova.getActivity().invalidateOptionsMenu();
             });
-            callbackContext.success("Menu shown");
-            return true;
         }
-        return false;
     }
 
-    // Wird von MainActivity weitergeleitet
     public void onCreateOptionsMenu(Menu menu) {
         this.menu = menu;
-        menu.clear(); // vorherige Items löschen
-
+        menu.clear();
         for (MenuItemData itemData : menuItems) {
             MenuItem item = menu.add(Menu.NONE, itemData.id, Menu.NONE, itemData.title);
             item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
         }
     }
 
-    // Wird von MainActivity weitergeleitet
     public boolean onOptionsItemSelected(MenuItem item) {
-        for (MenuItemData menuItem : menuItems) {
-            if (menuItem.id == item.getItemId()) {
-                webView.loadUrl("javascript:cordova.fireDocumentEvent('pluginMenuClicked', {id:" + menuItem.id + "});");
+        for (MenuItemData data : menuItems) {
+            if (data.id == item.getItemId()) {
+                webView.loadUrl("javascript:cordova.fireDocumentEvent('pluginMenuClicked',{id:" + data.id + "});");
                 return true;
             }
         }
         return false;
     }
-    
+
+    private static class MenuItemData {
+        int id;
+        String title;
+        MenuItemData(int id, String title) { this.id = id; this.title = title; }
+    }
 }
